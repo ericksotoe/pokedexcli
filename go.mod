30fd1261e5bf8232dfadb401bfcf030494a72654
ericksotoe <ericksotoe@gmail.com>
2026-01-06 23:03:25 +0900

    learned how to configure unit tests will add more, also working on simple repl proccess

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ericksotoe/pokedexcli
 
 go 1.25.5
+
+require github.com/google/go-cmp v0.7.0
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() // advances the scanner
+		userInput := scanner.Text()
+		userInputSlice := cleanInput(userInput) 
+		fmt.Printf("Your command was: <%s>", userInputSlice[0])
+	}
 }
+
+
