b215dbff217ffa45d3bb5edbff5409833a3cea2e
ericksotoe <ericksotoe@gmail.com>
2026-01-07 12:17:28 +0900

    added basic REPL proccess, adding commands to the prompt

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan() // advances the scanner
-		userInput := scanner.Text()
-		userInputSlice := cleanInput(userInput) 
-		fmt.Printf("Your command was: <%s>", userInputSlice[0])
-	}
+	startRepl()
 }
 
 
--- repl.go
+++ repl.go
@@ -1,6 +1,23 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() // advances the scanner
+		userInput := scanner.Text()
+		userInputSlice := cleanInput(userInput) 
+		fmt.Printf("Your command was: %s\n", userInputSlice[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
